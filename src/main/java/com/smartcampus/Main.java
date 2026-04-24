82bb049f31af6fd92b117b45af81f3ed9794c289
R1sayan <rasyn99@gmail.com>
2026-04-24 23:04:01 +0545

    Smart Campus API by Rasayan

---
+++ src/main/java/com/smartcampus/Main.java
@@ -0,0 +1,33 @@
+package com.smartcampus;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import java.net.URI;
+import java.io.IOException;
+
+public class Main {
+
+    public static final String BASE_URI = "http://localhost:8080/";
+
+    public static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig()
+                .packages("com.smartcampus");
+        return GrizzlyHttpServerFactory.createHttpServer(
+                URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println("=========================================");
+        System.out.println(" Smart Campus API - Started Successfully!");
+        System.out.println("=========================================");
+        System.out.println(" URL : http://localhost:8080/api/v1");
+        System.out.println(" Student : Rasayan");
+        System.out.println("=========================================");
+        System.out.println(" Press ENTER to stop the server...");
+        System.in.read();
+        server.stop();
+        System.out.println(" Server stopped.");
+    }
+}
---
+++ src/main/java/com/smartcampus/SmartCampusApplication.java
@@ -0,0 +1,9 @@
+package com.smartcampus;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class SmartCampusApplication extends Application {
+    // JAX-RS will auto-scan packages for resources
+}
